b4eaacf99fc9ea9d31d320cab20b425b25015b15
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2025-05-14 09:35:18 +1000

    deleted sample.test.ts

--- tests/sample.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test.skip('basic book store test', async ({ page }) => {
-  await page.goto('/books');
-  await expect(page).toHaveURL(/books/);
-  await expect(page.locator('.rt-table')).toBeVisible();
-});
